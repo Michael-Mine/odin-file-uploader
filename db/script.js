b8d77d2659ea02b1437095d16ce9b29e1b0d5e4c
Michael-Mine <147097601+Michael-Mine@users.noreply.github.com>
2026-03-08 12:36:54 +0000

    feat: apply migration, generate client, add script

---
+++ db/script.js
@@ -0,0 +1,24 @@
+import { prisma } from "./lib/prisma.js";
+
+async function main() {
+  // Create a new user with a post
+  const user = await prisma.user.create({
+    data: {
+      firstName: "Mr",
+      lastName: "Mine",
+      username: "m@m.net",
+      password: "123",
+    },
+  });
+  console.log("Created user:", user);
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
